f2ffbe948593147ecbd40fb5d8947980cd6e72c6
Jhild737 <46577086+Jhild737@users.noreply.github.com>
2021-10-01 09:39:29 -0400

    Adding router links to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home-page', component: AppModule },
+  { path: 'recipe-page', component: RecipePage },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
